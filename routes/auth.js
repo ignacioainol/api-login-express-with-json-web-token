4ec6af969d88c9523613e2f820309cdc03339a24
ignacioainol <iggnaxios@gmail.com>
2020-03-09 12:06:31 -0300

    login and register ok

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,50 @@
 const router = require('express').Router();
 const User = require('../model/User')
-
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema =  Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-});
+const bcrypt = require('bcryptjs');
+const {registerValidation, loginValidation} = require('../validation');
 
 router.post('/register', async (req,res) => {
-
     //Lets validate the data before we a user
-    const { error } = schema.validate(req.body);
+    const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    //Checking if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if(emailExist) return res.status(400).send('Email already exist')
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id});
     } catch (error) {
         res.status(400).send(err);
     }
+});
+
+router.post('/login', async (req,res) => {
+    const {error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the email exist
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(400).send('Email is not found');
+
+    //Passwordis correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid  password');
+
+    res.status(200).send('Logged in');
+
 });
 
 module.exports = router;
